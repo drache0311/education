04f310d989ffd245a872cad272a8af090d3ec487
YongBeomChoi <drache0311@naver.com>
2023-08-01 03:35:21 +0900

    Add React

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import React, { useEffect, useState } from 'react'
+import Header from './Header'
 
 const App = () => {
   return (
@@ -8,5 +9,47 @@ const App = () => {
     </>
   )
 }
+
+//
+// lifeCycle
+
+// before render , render , after render
+// empty
+// {}
+// {},[]
+// {},[state]
+
+//
+// 명령형 선언형
+
+// const add = () => {
+//   let total = 0
+//   const list = [1, 2, 3, 4, 5]
+//   for (let i = 0; i < list.length; i++) {
+//     total += list[i]
+//   }
+//   return total
+// }
+
+// const add = () => {
+//   return [1, 2, 3, 4, 5].reduce((prev, curr) => prev + curr)
+// }
+//
+// console.log(add())
+
+//
+// Component
+
+// <Header/>
+// <Header message={}/>
+
+//
+// Virtual DOM
+
+// const [timer, setTimer] = useState<number>(1)
+//
+// setInterval(() => {
+//     setTimer((prevState) => prevState + 1)
+// }, 2000)
 
 export default App
